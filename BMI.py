eab225af384b2ed60c4becf59c0a95fd26519268
JouniKiiskila <43674@edu.raseko.fi>
2021-02-12 09:23:21 +0200

    välipäivitys

---
+++ BMI.py
@@ -0,0 +1,71 @@
+
+# Funktio painoindeksin (body mass index) laskemiseksi
+def bmi(paino, pituus):
+    painoindeksi = paino / pituus**2
+    return painoindeksi
+
+
+
+# Funktio kehon rasvaprosentin laskemiseksi
+def rasvaprosentti(bmi, ika, sukupuoli):
+    rprosentti = 1.2 * bmi + 0.23 * ika - 10.8 * sukupuoli - 5.4
+    return rprosentti
+
+
+# Kysytään käyttäjältä tarvittavat tiedot
+paino_str = input("Anna painosi kilogrammoina: ")
+pituus_str = input("Anna pituutesi metreinä: ")
+ika_str = input("Kuinka vanha olet?: ")
+sukupuoli_str = input("Paina 1, jos olet mies, 0 jos olet nainen: ")
+
+
+# Merkkijonot luvuiksi
+paino = float(paino_str)
+pituus = float(pituus_str)
+ika = float(ika_str)
+sukupuoli = float(sukupuoli_str)
+
+
+
+# Lasketaan painoindeksi kysyttyjen tietojen perusteella
+bmi = bmi(paino, pituus)
+
+# Lasketaan rasvaprosentti
+rasvaprosentti = rasvaprosentti(bmi, ika, sukupuoli)
+print("Rasvaprosenttisi on", rasvaprosentti)
+
+try:
+    if paino_str.isalpha():
+        
+    paino = float(paino_str)
+except:
+    print('Paino Virheellinen')
+    tapahtui_virhe = True
+
+try:
+    pituus = float(pituus_str)
+except:
+    print('Pituus virheellinen')
+    tapahtui_virhe = True
+
+try:
+    ika = float(ika_str)
+except:
+    print('Ikä virheellinen')
+    tapahtui_virhe = True
+
+try:
+    sukupuoli = float(sukupuoli_str)
+except:
+    print('Sukupuoli virheellinen, syötä vain 0 tai 1')
+    tapahtui_virhe = True
+
+if tapahtui_virhe == False:
+    painoindeksi = bmi(paino, pituus)
+    print('Painoindeksisi on:', painoindeksi)
+    rprosentti = rasvaprosentti(painoindeksi, ika, sukupuoli)
+    print('Kehosi rasvaprosentti on:', rprosentti)
+else:
+    print('Ei voitu laske, koska syöttötiedoissa oli virhe')
+    
+  
